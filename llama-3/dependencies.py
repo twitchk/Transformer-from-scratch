d5d588e5c91741001c4388698325e0f2c8a73373
twitchk <35339266+twitchk@users.noreply.github.com>
2024-06-16 13:07:49 +0000

    Create dependencies.py

---
+++ llama-3/dependencies.py
@@ -0,0 +1,3 @@
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+pip install torch transformers
+pip install --no-deps packaging ninja einops flash-attn xformers trl peft accelerate bitsandbytes
